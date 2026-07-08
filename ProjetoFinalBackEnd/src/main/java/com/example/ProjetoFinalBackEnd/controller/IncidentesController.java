593ab198fa94cb8f53126aed6d1c647ea452d7ee
Geovane Ferreira <you@example.com>
2026-07-07 21:37:45 -0300

    Atualização no IncidentesController

--- ProjetoFinalBackEnd/src/main/java/com/example/ProjetoFinalBackEnd/controller/IncidentesController.java
+++ ProjetoFinalBackEnd/src/main/java/com/example/ProjetoFinalBackEnd/controller/IncidentesController.java
@@ -1,4 +1,46 @@
 package com.example.ProjetoFinalBackEnd.controller;
 
+
+import com.example.ProjetoFinalBackEnd.dto.IncidentesRequestDTO;
+import com.example.ProjetoFinalBackEnd.dto.IncidentesResponseDTO;
+import com.example.ProjetoFinalBackEnd.service.IncidentesService;
+import jakarta.validation.Valid;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Map;
+
+@RestController
+@RequestMapping("/incidentes")
+//@CrossOrigin(origins = "http://localhost:5173")
 public class IncidentesController {
+
+    @Autowired
+    private IncidentesService service;
+
+    @GetMapping
+    public ResponseEntity<List<IncidentesResponseDTO>> listar(){
+        return ResponseEntity.status(HttpStatus.OK).body(service.listar());
+    }
+
+    @PostMapping
+    public ResponseEntity<Map<String, Object>> salvar(@RequestBody @Valid IncidentesRequestDTO salvarDTO){
+        service.salvar(salvarDTO);
+        return ResponseEntity.status(HttpStatus.CREATED).body(Map.of("Mensagem", "Registro Realizado com Sucesso. ✅"));
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<Map<String, Object>> atualizar(@PathVariable Long id, @RequestBody @Valid IncidentesRequestDTO salvarDTO){
+        service.atualizar(id, salvarDTO);
+        return ResponseEntity.status(HttpStatus.OK).body(Map.of("Mensagem", "Registro Atualizado com Sucesso. ✅"));
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Map<String, Object>> deletar(@PathVariable Long id){
+        service.deletar(id);
+        return ResponseEntity.status(HttpStatus.OK).body(Map.of("Mensagem", "Registro deletado com Sucesso. ✅"));
+    }
 }
