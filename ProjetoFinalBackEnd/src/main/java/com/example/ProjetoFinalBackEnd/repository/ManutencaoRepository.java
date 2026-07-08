a357b2ae88ea56ecbaafdc8ad5af62bf537a58b2
Michell Silva Santos <15558@pcivil.intranet>
2026-07-08 15:36:25 -0300

    feat:Subindo Regras de Negócio Extras no ManutencaoRepository

--- ProjetoFinalBackEnd/src/main/java/com/example/ProjetoFinalBackEnd/repository/ManutencaoRepository.java
+++ ProjetoFinalBackEnd/src/main/java/com/example/ProjetoFinalBackEnd/repository/ManutencaoRepository.java
@@ -1,11 +1,29 @@
 package com.example.ProjetoFinalBackEnd.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import com.example.ProjetoFinalBackEnd.model.ManutencaoModel;
+import com.example.ProjetoFinalBackEnd.model.enums.Criticidade;
+
 
 @Repository
 public interface ManutencaoRepository extends JpaRepository<ManutencaoModel,Long> {
+
+    //feat:Adicionando regras de negócio extras para futuras utilidades:
+
+    //1: Buscar todo o histórico de manutenção de um determinado equipamento (ex: 'TC-1023')
+
+    List<ManutencaoModel> findByEquipamentoId(String equipamentoId);
+
+    //2: Filtrar manutenções por nível de urgência (ex: Listar apenas as 'CRÍTICA')
+
+    List<ManutencaoModel> findByCriticidade(Criticidade criticidade);
+
+    //3: Verificar se já existe alguma solicitação CRÍTICA aberta para o mesmo equipamento
+
+    boolean existsByEquipamentoIdAndCriticidade(String equipamentoId, Criticidade criticidade);
     
 }
