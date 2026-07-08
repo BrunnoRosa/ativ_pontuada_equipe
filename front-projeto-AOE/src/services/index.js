9f0de80fa6e751a426d53f5bf34e714ed5ceaf9b
Michell Silva Santos <15558@pcivil.intranet>
2026-07-08 13:39:53 -0300

    feat:Adicionando o Axios(no index.jsx) para funcionamento da integração

---
+++ front-projeto-AOE/src/services/index.js
@@ -0,0 +1,20 @@
+import axios from 'axios';
+
+//Criação da instância centralizada do Axios
+
+const api = axios.create({
+    baseURL: 'http/localhost:8080/api',
+    headers: {
+        'Content-Type' : 'application/json'
+    }
+});
+
+//Função para chamar especificamente a rota de manutenção
+
+export const criarOrdemServico = async(dadosManutencao) => {
+    const response = await api.post('/manutencoes', dadosManutencao);
+
+    return response.data;
+}
+
+export default api;
