361a04c0b8684885d48b5f688ce8e4515f252dec
michellfloat <michell.ssfloat@gmail.com>
2026-07-07 21:04:58 -0300

    Subindo DTOs iniciais da Manutenção

--- ProjetoFinalBackEnd/src/main/java/com/example/ProjetoFinalBackEnd/repository/ManutencaoRepository.java
+++ ProjetoFinalBackEnd/src/main/java/com/example/ProjetoFinalBackEnd/repository/ManutencaoRepository.java
@@ -1,4 +1,11 @@
 package com.example.ProjetoFinalBackEnd.repository;
 
-public interface ManutencaoRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.example.ProjetoFinalBackEnd.model.ManutencaoModel;
+
+@Repository
+public interface ManutencaoRepository extends JpaRepository<ManutencaoModel,Long> {
+    
 }
